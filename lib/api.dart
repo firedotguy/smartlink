e521bc59db0a0f4f12baa8c37c1423f0ea77b80c
TimurNasirov <nta16022013@gmail.com>
2025-09-17 18:22:09 +0300

    refactor: api functions

--- lib/api.dart
+++ lib/api.dart
@@ -1,97 +1,162 @@
 import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:smartlink/main.dart';
 
-/// API key
-final String api = dotenv.env['API_BASE']!;
-/// API domain
-final String key = dotenv.env['API_KEY']!;
+final String _base = dotenv.env['API_BASE']!;
+final String _key  = dotenv.env['API_KEY']!;
+final Client _client = Client();
 
+Uri _u(String action, Map<String, String> qp, {bool direct = false}) {
+  final params = {'apikey': _key, ...qp};
+  final base = Uri.parse(_base);
+  final act = action.startsWith('/') ? action.substring(1) : action;
 
-/// get customer data
+  Uri uri;
+  if (direct) {
+    final origin = Uri(
+      scheme: base.scheme,
+      host: base.host,
+      port: base.hasPort ? base.port : null,
+    );
+    uri = origin.replace(path: act, queryParameters: params);
+  } else {
+    final basePath = base.path.replaceAll(RegExp(r'/+$'), '');
+    uri = base.replace(path: '$basePath/$act', queryParameters: params);
+  }
+
+  l.d('API: action="$action" qp=$qp direct=$direct -> $uri');
+  return uri;
+}
+
+Future<Map<String, dynamic>> _get(String action, Map<String, String> qp, {bool direct = false}) async {
+  l.d('API GET: $action $qp');
+  final uri = _u(action, qp, direct: direct);
+  final resp = await _client.get(uri).timeout(const Duration(seconds: 20));
+
+  if (resp.statusCode != 200) {
+    l.e('API GET $action -> HTTP ${resp.statusCode}, body: ${resp.body}');
+    throw Exception('HTTP ${resp.statusCode}');
+  }
+  final data = jsonDecode(resp.body);
+  if (data is Map<String, dynamic>) return data;
+
+  l.e('API GET unexpected response type for $action');
+  throw Exception('Unexpected response');
+}
+
+Future<Map<String, dynamic>> _post(String action, Map<String, String> qp, {bool direct = false}) async {
+  l.d('API POST: $action $qp');
+  final uri = _u(action, qp, direct: direct);
+  final resp = await _client.post(uri).timeout(const Duration(seconds: 20));
+
+  if (resp.statusCode != 200) {
+    l.e('API POST $action -> HTTP ${resp.statusCode}, body: ${resp.body}');
+    throw Exception('HTTP ${resp.statusCode}');
+  }
+  final data = jsonDecode(resp.body);
+  if (data is Map<String, dynamic>) return data;
+
+  l.e('API POST unexpected response type for $action');
+  throw Exception('Unexpected response');
+}
+
 Future<Map<String, dynamic>> getCustomer(int id) async {
-  l.i('API: get customer data, customerId: $id');
-  final res = await http.get(Uri.parse('$api/customer?id=$id&apikey=$key'));
-  return jsonDecode(res.body);
+  l.i('API: get customer, id=$id');
+  return await _get('customer/$id', {});
 }
 
-/// get box data
 Future<Map<String, dynamic>> getBox(int id) async {
-  l.i('API: get box data, boxId: $id');
-  final res = await http.get(Uri.parse('$api/box?id=$id&apikey=$key'));
-  return jsonDecode(res.body);
+  l.i('API: get box, id=$id');
+  return await _get('box/$id', {});
 }
 
-/// get customer attachments
-Future<Map<String, dynamic>> getAttach(int id) async {
-  l.i('API: get attachments, customerId: $id');
-  final res = await http.get(Uri.parse('$api/attachs?id=$id&apikey=$key'));
-  return jsonDecode(res.body);
+Future<Map<String, dynamic>> getAttach(int customerId) async {
+  l.i('API: get attachs, customerId=$customerId');
+  return await _get('attachs/customer/$customerId', {
+    'include_task': 'true',
+  });
 }
 
-/// get task comments
-Future<Map<String, dynamic>> getComments(int id) async {
-  l.i('API: get comments, taskId: $id');
-  final res = await http.get(Uri.parse('$api/comments?id=$id&apikey=$key'));
-  return jsonDecode(res.body);
-}
-
-/// find customer
 Future<List<Map>> find(String query) async {
-  l.i('API: find customers, query: $query');
-  final res = await http.get(Uri.parse('$api/find?query=$query&apikey=$key'));
-  return List<Map>.from(jsonDecode(res.body)['customers']);
+  l.i('API: find customers, query="$query"');
+  final raw = await _get('customer/search', {'query': query});
+  return List<Map>.from(raw['customers'] ?? const []);
 }
 
-/// check login
 Future<Map> login(String login, String password) async {
-  l.i('API: check login, login: $login, password: $password');
-  final res = await http.get(Uri.parse('$api/login?login=$login&password=$password&apikey=$key'));
-  return jsonDecode(res.body);
+  l.i('API: employee login, login="$login"');
+  return await _get('employee/login', {
+    'login': login,
+    'password': password,
+  });
 }
 
-/// get additional data values
 Future<Map> getAdditionalData() async {
   l.i('API: get additional data');
-  final res = await http.get(Uri.parse('$api/additional_data?apikey=$key'));
-  return jsonDecode(res.body)['data'];
+  final raw = await _get('addata/options', {});
+  return Map<String, dynamic>.from(raw['data'] ?? const {});
 }
 
-/// get divisions list
 Future<List<Map>> getDivisions() async {
   l.i('API: get divisions');
-  final res = await http.get(Uri.parse('$api/divisions?apikey=$key'));
-  return List<Map>.from(jsonDecode(res.body)['data']);
+  final raw = await _get('employee/divisions', {});
+  return List<Map>.from(raw['data'] ?? const []);
 }
 
-/// create task
-Future<int> createTask(int customerId, int authorId, String reason, bool box, int? boxId, String description, List<int> divisions, String phone, String type) async {
-  l.i('API: create task');
-  final res = await http.post(Uri.parse('$api/task?apikey=$key&customer_id=$customerId&author_id=$authorId&reason=$reason&box=$box&description=$description&divisions=$divisions&phone=$phone&type=$type${boxId != null? '&box_id=$boxId' : ""}'));
-  return jsonDecode(res.body)['id'];
+Future<int> createTask(
+  int customerId,
+  int authorId,
+  String reason,
+  bool box,
+  int? boxId,
+  String description,
+  List<int> divisions,
+  String phone,
+  String type,
+) async {
+  l.i('API: create task for customer=$customerId');
+  final qp = <String, String>{
+    'customer_id': customerId.toString(),
+    'author_id': authorId.toString(),
+    'reason': reason,
+    'box': box.toString(),
+    if (boxId != null) 'box_id': boxId.toString(),
+    'description': description,
+    'divisions': divisions.join(','),
+    'phone': phone,
+    'type': type,
+  };
+  final raw = await _post('task', qp);
+  final id = raw['id'];
+  if (id is int) return id;
+  throw Exception('Task id missing');
 }
 
-/// get employee name
 Future<String> getEmployeeName(int id) async {
-  l.i('API: get employee name');
-  final res = await http.get(Uri.parse('$api/employee/name?apikey=$key&id=$id'));
-  return jsonDecode(res.body)['name'];
+  l.i('API: get employee name, id=$id');
+  final raw = await _get('employee/name', {'id': id.toString()});
+  return (raw['name'] ?? '').toString();
 }
 
-/// get ont&olt data
 Future<Map> getOnt(int oltId, String sn) async {
-  l.i('API: get ont data');
-  final res = await http.get(Uri.parse('$api/ont?apikey=$key&olt_id=$oltId&sn=$sn'));
-  return jsonDecode(res.body);
+  l.i('API: get ont data, oltId=$oltId sn=$sn');
+  return await _get('ont', {
+    'olt_id': oltId.toString(),
+    'sn': sn,
+  });
 }
 
-/// restart ONT
 Future<String?> restartOnt(int ontId, String host, Map interface) async {
-  l.i('API: restart ont');
-  final res = await http.get(Uri.parse('$api/ont/restart?apikey=$key&id=$ontId&host=$host&fibre=${interface['fibre']}&service=${interface['service']}&port=${interface['port']}'));
-  if (jsonDecode(res.body)['status'] != 'success'){
-    return jsonDecode(res.body)['detail'] ?? 'unknown error';
-  }
-  return null;
+  l.i('API: restart ONT id=$ontId host=$host iface=$interface');
+  final raw = await _get('ont/restart', {
+    'id': ontId.toString(),
+    'host': host,
+    'fibre': '${interface['fibre']}',
+    'service': '${interface['service']}',
+    'port': '${interface['port']}',
+  });
+  if (raw['status'] == 'success') return null;
+  return (raw['detail'] ?? 'unknown error').toString();
 }
